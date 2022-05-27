35e56a279f1e0357c9dbd79f34468d9225086363
zLhYd <adigans221@gmail.com>
2022-05-27 10:29:40 +0700

    second change

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import Nav from "./components/navbar/Nav";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Project from "./components/project/Project";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Router>
+      <BrowserRouter>
         <Nav />
         <Routes>
           <Route exact path="/bro" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/project" element={<Project />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 };
