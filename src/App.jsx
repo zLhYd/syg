41b778b481327ed168c417fce27e1c49a4e218f4
zLhYd <adigans221@gmail.com>
2022-05-27 10:11:11 +0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <Router>
         <Nav />
         <Routes>
-          <Route path="/bro" element={<Home />} />
+          <Route exact path="/bro" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/project" element={<Project />} />
         </Routes>
